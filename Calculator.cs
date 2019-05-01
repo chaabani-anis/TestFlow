4cf660cc0a2e59214d4203301b85e9f96d73eba3
Anis <chaabani.anis@gmail.com>
2019-05-01 18:52:37 +0200

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace TestFlow
+{
+    public class Calculator
+    {
+        private IList<int> _listeValeurs;
+        private int _result;
+
+        public Calculator()
+        {
+            _listeValeurs = new List<int>();
+            _result = 0;
+        }
+
+        public void SetInput(int value)
+        {
+            _listeValeurs.Add(value);
+        }
+
+        public void Add()
+        {
+            foreach (var value in _listeValeurs)
+            {
+                _result += value;
+            }
+        }
+
+        public int GetResult()
+        {
+            return _result;
+        }
+    }
+}
---
+++ SpecFlowFeatureSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace TestFlow
+{
+    [Binding]
+    public class SpecFlowFeatureSteps
+    {
+        Calculator _calculator; //= new Calculator();
+
+        public SpecFlowFeatureSteps(Calculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int value)
+        {
+            _calculator.SetInput(value);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _calculator.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int value)
+        {
+            int result = _calculator.GetResult();
+            Assert.AreEqual(value, result);
+        }
+    }
+}
